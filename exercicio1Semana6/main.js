5e31758170b90662fee74afa86279aecbdb65b24
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-09 10:49:33 -0300

    criando o arquivo main.js, importando as funções

---
+++ exercicio1Semana6/main.js
@@ -0,0 +1,9 @@
+import {suma} from "./somaArray.js"
+import {mediaCalc} from "./calculaMedia.js"
+import {multiplica} from "./multiplicaArray.js"
+
+let exe=[5,36,52,55,25,52];
+let exe2=[5,2]
+suma(exe)
+mediaCalc(exe)
+multiplica(exe2)
