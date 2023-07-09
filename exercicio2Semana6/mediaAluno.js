d2c555af88f33072eefb34e0891809353c5ed88c
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-09 19:48:01 -0300

    função .map  e novo array [nome,media]

--- exercicio2Semana6/mediaAluno.js
+++ exercicio2Semana6/mediaAluno.js
@@ -12,3 +12,12 @@ let aluno3= new aluno('Hernan',10,9)
 let aluno4= new aluno("Mauricio",5,8)
 
 const alunos=[aluno1,aluno2,aluno3,aluno4]
+
+const mediaAluno = alunos.map(aluno => {
+    const media = (aluno.nota1 + aluno.nota2) / 2;
+    return {
+      nome: aluno.nome,
+      media: media
+    };
+  });
+  console.log(mediaAluno)
