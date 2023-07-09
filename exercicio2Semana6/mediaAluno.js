5c2a57125d3067dc2ce79a761df899c5d33712ff
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-09 19:39:59 -0300

    criando array de alunos

---
+++ exercicio2Semana6/mediaAluno.js
@@ -0,0 +1,14 @@
+class aluno{
+    constructor(nome,nota1,nota2){
+    this.nome=nome
+    this.nota1=nota1
+    this.nota2=nota2
+    }
+}
+
+let aluno1= new aluno('Maria',8,5)
+let aluno2= new aluno("João",7,9)
+let aluno3= new aluno('Hernan',10,9)
+let aluno4= new aluno("Mauricio",5,8)
+
+const alunos=[aluno1,aluno2,aluno3,aluno4]
