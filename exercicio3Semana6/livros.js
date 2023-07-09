1e95e5b5916e5f86f84f2f14e8c1c7254a900825
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-09 14:49:34 -0300

    criando tres objetos classe livro

--- exercicio3Semana6/livros.js
+++ exercicio3Semana6/livros.js
@@ -8,4 +8,8 @@ class livro {
         console.log(`O livro ${this.titulo} do autor${this.autor} foi publicado no ano ${this.anoPublicacao} .`)
          }
 }
-    
+let livro1= new livro("Moby Dyck"," Herman Melville",1851)    
+let livro2= new livro("O Senhor dos Anéis","J.R.R. Tolkien",1955)
+let livro3= new livro("1984","George Orwell",1949)
+
+
