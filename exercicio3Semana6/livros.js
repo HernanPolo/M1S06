2e185f8aa53b9c22cb5ad64868e1ca57e752ccd9
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-09 14:53:09 -0300

    executando o metodo obterInformacoes

--- exercicio3Semana6/livros.js
+++ exercicio3Semana6/livros.js
@@ -5,11 +5,12 @@ class livro {
     this.anoPublicacao=anoPublicacao
     }
     obterInformacoes(){
-        console.log(`O livro ${this.titulo} do autor${this.autor} foi publicado no ano ${this.anoPublicacao} .`)
-         }
+        console.log(`O livro ${this.titulo} do autor ${this.autor} foi publicado no ano ${this.anoPublicacao}.`)
+    }
 }
-let livro1= new livro("Moby Dyck"," Herman Melville",1851)    
+let livro1= new livro("Moby Dyck","Herman Melville",1851)    
 let livro2= new livro("O Senhor dos Anéis","J.R.R. Tolkien",1955)
 let livro3= new livro("1984","George Orwell",1949)
-
-
+livro1.obterInformacoes()
+livro2.obterInformacoes()
+livro3.obterInformacoes()
