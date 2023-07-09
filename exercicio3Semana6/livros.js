d1d001366a05614c4566901b193f3a397b06b809
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-09 14:47:24 -0300

    implemetntando metodo obterInformacoes

--- exercicio3Semana6/livros.js
+++ exercicio3Semana6/livros.js
@@ -4,5 +4,8 @@ class livro {
     this.autor=autor
     this.anoPublicacao=anoPublicacao
     }
+    obterInformacoes(){
+        console.log(`O livro ${this.titulo} do autor${this.autor} foi publicado no ano ${this.anoPublicacao} .`)
+         }
 }
     
