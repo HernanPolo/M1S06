78ca54d155db756a996142f60c580aa5b6d3e695
Hernan Polo <hernan.polo1985@gmail.com>
2023-07-12 18:52:14 -0300

    modulo principal import somaImpar import soPares executa as funcoes

---
+++ exercicio5Semana6/main.js
@@ -0,0 +1,7 @@
+import {somaImpar} from "./somaImpares.js"
+import {soPares} from "./filtrarPares.js"
+  
+let num =[5,2,2,1,4,8,6,2,9,7,12,65,14]
+console.log (`Lista de numeros escolhidos pelo usuario: ${num}`)
+soPares(num)
+somaImpar(num)
